26991eca7fbdbbf89a59b8f60ff251d88bc83904
VoidFrog <shadownn0000@gmail.com>
2025-10-22 12:25:40 +0200

    fix: imported package doesnt need the unplugin, as it shouldnt have from the start - changed slot

--- src/enums/waveCallback.ts
+++ src/enums/waveCallback.ts
@@ -2,18 +2,20 @@ import tgpu, { type TgpuFn } from 'typegpu';
 import * as std from 'typegpu/std';
 import * as d from 'typegpu/data';
 
+const defaultWave = (pos: d.v2f) => {
+  'kernel';
+
+  const x = pos.x;
+  const y = pos.y;
+
+  const waveX = std.sin(x * 2.0);
+  const waveY = std.cos(y * 2.0);
+
+  return d.vec2f(waveX, waveY);
+};
+
 export const WAVE_CALLBACKS = {
-  default: (pos: d.v2f) => {
-    'use gpu';
-
-    const x = pos.x;
-    const y = pos.y;
-
-    const waveX = std.sin(x * 2.0);
-    const waveY = std.cos(y * 2.0);
-
-    return d.vec2f(waveX, waveY);
-  },
+  default: defaultWave,
 } as const;
 
 export const waveCallbackSlot = tgpu.slot<TgpuFn<(pos: d.Vec2f) => d.Vec2f>>();
